74340893cf42323579e50d9521f9d4f129e67a41
TranqVi <142180486+TranqVi@users.noreply.github.com>
2023-08-14 02:39:42 +0300

    Create getBlogs.ts

---
+++ src/app/actions/getBlogs.ts
@@ -0,0 +1,21 @@
+import prisma from "@/app/lib/prismadb"
+
+export default async function getBlogs(){
+    try {
+        const blogs = await prisma.blog.findMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+
+        const safeBlogs = blogs.map((blog) => ({
+            ...blog,
+            createdAt: blog.createdAt.toISOString()
+        }))
+
+        return safeBlogs
+
+    } catch (err:any) {
+        throw new Error(err);
+    }
+}
